aa36704666186ccbb063ffe99b2e8eba389665b4
djanr2 <djanr2@hotmail.com>
2023-06-18 23:58:45 -0600

    20230618 Se agregan las rutas para los elementos del header, una estatica y una lazy, se configura ruta de default y wildldcard route, se generan modulos de contact y articles

--- skully-jamstack-spa/src/app/app.module.ts
+++ skully-jamstack-spa/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { ContactModule } from "./contact/contact.module";
 
 @NgModule({
   declarations: [
@@ -12,9 +13,10 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, 
+    AppRoutingModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    ContactModule
   ],
   providers: [],
   bootstrap: [AppComponent]
