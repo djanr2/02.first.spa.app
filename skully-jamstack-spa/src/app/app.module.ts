7fd4cf403fe6dafa6e92d2e5beb18a3da10d581f
djanr2 <djanr2@hotmail.com>
2023-06-20 23:37:15 -0600

    20230620 Adding scully to project to generate easily a personal blog

--- skully-jamstack-spa/src/app/app.module.ts
+++ skully-jamstack-spa/src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-import { ContactModule } from "./contact/contact.module";
+import { ContactModule } from './contact/contact.module';
+import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ContactModule } from "./contact/contact.module";
     AppRoutingModule,
     CoreModule,
     SharedModule,
-    ContactModule
+    ContactModule,
+    ScullyLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
